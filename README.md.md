cfa200fa25eb7170461cde95f5519499e46999aa
jaba-jessy <jabar@uit.ac.in>
2026-07-10 22:07:47 +0530

    Add files via upload

---
+++ README.md.md
@@ -0,0 +1,17 @@
+# Password Strength Checker
+
+## Description
+This is a simple Python project that checks whether a password is Weak, Medium, or Strong.
+
+## Features
+- Checks password length
+- Checks uppercase letters
+- Checks lowercase letters
+- Checks numbers
+- Checks special characters
+
+## Language Used
+- Python 3
+
+## Author
+jaba
---
+++ password-strength-checker.py
@@ -0,0 +1,12 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/jabaj/OneDrive/Desktop/password-strength-checker/password-strength-checker.py
+================================
+   PASSWORD STRENGTH CHECKER
+================================
+Enter your password: hello123
+
+Password Analysis
+-------------------------
+Password Strength: MEDIUM
