92b1206bbe37c413cc17448d53ff8d1b562d8c15
jaba-jessy <jabar@uit.ac.in>
2026-07-10 22:50:08 +0530

    Enhance password strength checking logic
    Refactored password strength checker to improve functionality and readability.

--- password-strength-checker.py
+++ password-strength-checker.py
@@ -1,12 +1,32 @@
-Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-= RESTART: C:/Users/jabaj/OneDrive/Desktop/password-strength-checker/password-strength-checker.py
-================================
-   PASSWORD STRENGTH CHECKER
-================================
-Enter your password: hello123
+import re
 
-Password Analysis
--------------------------
-Password Strength: MEDIUM
+print("===== PASSWORD STRENGTH CHECKER =====")
+
+password = input("Enter your password: ")
+
+strength = 0
+
+if len(password) >= 8:
+    strength += 1
+
+if re.search(r"[A-Z]", password):
+    strength += 1
+
+if re.search(r"[a-z]", password):
+    strength += 1
+
+if re.search(r"[0-9]", password):
+    strength += 1
+
+if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+    strength += 1
+
+print("\nPassword Analysis")
+print("----------------------")
+
+if strength <= 2:
+    print("Password Strength: WEAK")
+elif strength <= 4:
+    print("Password Strength: MEDIUM")
+else:
+    print("Password Strength: STRONG")
